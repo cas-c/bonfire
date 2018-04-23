94dfafd79fcaa5098478f22a355fec3f544d8751
gloss-water <gloss.water@gmail.com>
2018-04-23 12:39:51 -0600

    simplify switcher return

--- utils/responses.js
+++ utils/responses.js
@@ -2,31 +2,31 @@ const switcher = cases => defaultCase => key => cases.hasOwnProperty(key) ? case
 
 const responses = args => response_code => {
     return switcher({
-        'GO_AWAY': () => { return {
+        'GO_AWAY': () => ({
             success: false,
             message: 'Not authenticated.',
             code: 'GO_AWAY'
-        }},
-        'NO_USER_GIVER': () => { return {
+        }),
+        'NO_USER_GIVER': () => ({
             success: false,
             message: `User ${args[0]} does not exist in our database.`,
             code: 'NO_USER_GIVER'
-        }},
-        'NO_USER_RECEIVER': () => { return {
+        }),
+        'NO_USER_RECEIVER': () => ({
             success: false,
             message: `User ${args[0]} does not exist in our database.`,
             code: 'NO_USER_RECEIVER'
-        }},
-        'TOO_BROKE_LOL': () => { return {
+        }),
+        'TOO_BROKE_LOL': () => ({
             success: false,
             message: `User ${args[0]} does not have enough funds for this transaction.`,
             code: 'TOO_BROKE_LOL'
-        }},
-        'SUCCESSFUL_TRANSFER': () => { return {
+        }),
+        'SUCCESSFUL_TRANSFER': () => ({
             success: true,
             message: `${args[0]} has given ${args[1]} ${args[2]} souls.`,
             code: 'SUCCESSFUL_TRANSFER'
-        }}
+        })
     })({
         success: false,
         message: 'Error returning response.',
